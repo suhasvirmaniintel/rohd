a85a4bec603bc0d4143163e68fca2b8d3d092f60
Max Korbel <max.korbel@intel.com>
2024-11-25 09:17:33 -0800

    test that reproduces #531 issue, async reset sampling

--- test/sequential_test.dart
+++ test/sequential_test.dart
@@ -8,6 +8,8 @@
 // Substantial portion of test contributed by wswongat in https://github.com/intel/rohd/issues/79
 // Max Korbel <max.korbel@intel.com>
 
+import 'dart:async';
+
 import 'package:rohd/rohd.dart';
 import 'package:rohd/src/utilities/simcompare.dart';
 import 'package:test/test.dart';
@@ -207,5 +209,36 @@ void main() {
 
     await SimCompare.checkFunctionalVector(dut, vectors);
     SimCompare.checkIverilogVector(dut, vectors);
+  });
+
+  test('async reset samples correct reset value', () async {
+    final clk = Logic();
+    final reset = Logic();
+    final val = Logic();
+
+    reset.inject(0);
+    clk.inject(0);
+
+    Sequential.multi(
+      [clk, reset],
+      reset: reset,
+      [
+        val < 1,
+      ],
+    );
+
+    Simulator.registerAction(10, () {
+      clk.inject(1);
+    });
+
+    Simulator.registerAction(14, () {
+      reset.inject(1);
+    });
+
+    Simulator.registerAction(15, () {
+      expect(val.value.toInt(), 0);
+    });
+
+    await Simulator.run();
   });
 }
